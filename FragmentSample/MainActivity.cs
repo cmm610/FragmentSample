2a240c7d1aa67ab56275a5b0aab53c0d4187a6bf
Cara McIlnay <cara.mcilnay@telescope.tv>
2017-01-03 08:05:32 -0800

    init commit with all files

---
+++ FragmentSample/MainActivity.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace FragmentSample {
+	[Activity(Label = "Fragment Walkthrough", MainLauncher = true, Icon = "@drawable/launcher")]
+	public class MainActivity : Activity {
+		int count = 1;
+
+		protected override void OnCreate(Bundle savedInstanceState) {
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.activity_main);
+		}
+	}
+}
+
---
+++ FragmentSample/TitlesFragment.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace FragmentSample {
+	public class TitlesFragment : Android.App.ListFragment {
+		public override void OnCreate(Bundle savedInstanceState) {
+			base.OnCreate(savedInstanceState);
+
+			var adapter = new ArrayAdapter<String>(Activity, Android.Resource.Layout.SimpleListItemChecked, Shakespeare.Titles);
+			ListAdapter = adapter;
+			if (savedInstanceState != null) {
+				_currentPlayId = savedInstanceState.GetInt("current_play_id", 0);
+			}
+
+			var detailsFrame = Activity.FindActivityByViewId<View>(Resource.Id.details);
+			_isDualPane = detailsFrame != null && detailsFrame.Visibility == ViewStates.Visible;
+
+			if (_isDualPane) {
+				ListView.ChoiceMode = (int) ChoiceMode.Single;
+				ShowDetails(_currentPlayId);
+			}
+		}
+
+		public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+			// Use this to return your custom view for this Fragment
+			// return inflater.Inflate(Resource.Layout.YourFragment, container, false);
+
+			return base.OnCreateView(inflater, container, savedInstanceState);
+		}
+	}
+}
