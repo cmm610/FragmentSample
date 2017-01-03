dd9da3b804219c8a3dec45bafb06cba8c9cdae0d
Cara McIlnay <cara.mcilnay@telescope.tv>
2017-01-03 08:39:39 -0800

    added some more about to start on DetailsActivity

---
+++ FragmentSample/Resources/DetailsActivity.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace FragmentSample {
+	[Activity(Label = "DetailsActivity")]
+	public class DetailsActivity : Activity {
+		protected override void OnCreate(Bundle savedInstanceState) {
+			base.OnCreate(savedInstanceState);
+
+			var index = Intent.Extras.GetInt("current_play_id", 0);
+
+			var details
+		}
+	}
+}
--- FragmentSample/TitlesFragment.cs
+++ FragmentSample/TitlesFragment.cs
@@ -14,7 +14,13 @@ using Android.Widget;
 
 namespace FragmentSample {
 	public class TitlesFragment : Android.App.ListFragment {
-		public override void OnCreate(Bundle savedInstanceState) {
+		private int _currentPlayId;
+
+		private bool _isDualPane = false;
+
+
+
+		public override void OnActivityCreated(Bundle savedInstanceState) {
 			base.OnCreate(savedInstanceState);
 
 			var adapter = new ArrayAdapter<String>(Activity, Android.Resource.Layout.SimpleListItemChecked, Shakespeare.Titles);
@@ -23,7 +29,7 @@ namespace FragmentSample {
 				_currentPlayId = savedInstanceState.GetInt("current_play_id", 0);
 			}
 
-			var detailsFrame = Activity.FindActivityByViewId<View>(Resource.Id.details);
+			var detailsFrame = Activity.FindViewById<View>(Resource.Id.details);
 			_isDualPane = detailsFrame != null && detailsFrame.Visibility == ViewStates.Visible;
 
 			if (_isDualPane) {
@@ -37,6 +43,35 @@ namespace FragmentSample {
 			// return inflater.Inflate(Resource.Layout.YourFragment, container, false);
 
 			return base.OnCreateView(inflater, container, savedInstanceState);
+		}
+
+		public override void OnListItemClick(ListView view1, View v, int position, long id) {
+			ShowDetails(position);
+		}
+
+		private void ShowDetails(int playId) {
+			//how can i reference something that is not declared yet?
+			_currentPlayId = playId;
+			if (_isDualPane) {
+				ListView.SetItemChecked(playId, true);
+
+				var details = FragmentManager.FindFragmentById(Resource.Id.details) as DetailsFragment;
+				if (details == null || details.ShownPlayId != playId) {
+					details = new DetailsFragment.NewInstance(playId);
+
+					var ft = FragmentManager.BeginTransaction();
+					ft.Replace(Resource.Id.details, details);
+           			ft.SetTransition(FragmentTransaction.TransitFragmentFade);
+           			ft.Commit();
+				}
+			} else {
+		       // Otherwise we need to launch a new Activity to display
+		       // the dialog fragment with selected text.
+		       var intent = new Intent();
+		       intent.SetClass(Activity, typeof (DetailsActivity));
+		       intent.PutExtra("current_play_id", playId);
+		       StartActivity(intent);
+		   }
 		}
 	}
 }
